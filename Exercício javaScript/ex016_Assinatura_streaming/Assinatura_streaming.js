d4ced8e82d004ed8d9655726aa69af96636d9856
DanielHenriqueBezerra <danielxt499@gmail.com>
2025-08-15 20:51:38 -0300

    Subindo

---
+++ Exercício javaScript/ex016_Assinatura_streaming/Assinatura_streaming.js
@@ -0,0 +1,4 @@
+let opcaoPlano = prompt("Escolha uma das opções:\n[ 1 ]Básico\n[ 2 ] Premium\n[ 3 ]Ultra.")
+let opcaoTela = prompt("Quantas telas você deseja\n1-2 R$0,00\n3-4 R$5,00 por tela extra\n4 + R$7,50 por tela extra.")
+
+// if(){}
