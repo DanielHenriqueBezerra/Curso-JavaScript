2af724887717c1aca80b7260945aa1e74bf005c8
DanielHenriqueBezerra <danielxt499@gmail.com>
2025-08-20 20:35:51 -0300

    25

---
+++ Exercício javaScript/ex019_Lista_Vazia_nomes_de_cidades/Lista_Vazia_nomes_de_cidades.js
@@ -0,0 +1,24 @@
+// var array = [`valor1`,`valor2`, `valor3`,`valor4`,`valor5`]
+//     for (let i = 0; i < array.length; i++){
+//         console.log(i)
+//     }
+
+
+// for (let i = 1; i <= 10; i += 1)
+//     console.log("Contando:" + i);
+
+// const numeros = [1,2,3,4,5,6,7,8,9,10]
+
+// for (let i = 0 ; i < numeros.length; i++){
+
+//         if (numeros[i] % 2 !== 0){
+//             console.log(numeros[i]);
+            
+//         }
+        
+// }
+
+const animais = ["Cachorro", "Gato", "Pássaro"];
+    for (let i = 0; i < animais.length; i++){
+        console.log(`${i + 1}º. ${animais[i]}`);
+    }
