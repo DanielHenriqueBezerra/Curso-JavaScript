de650c18ac393e5edf57bc80a1f43dfdd974f264
DanielHenriqueBezerra <danielxt499@gmail.com>
2025-08-21 21:02:10 -0300

    Update Verificador_de_senha_while.js

--- Exercício javaScript/ex023_Verificador_de_senha_com_while copy/Verificador_de_senha_while.js
+++ Exercício javaScript/ex023_Verificador_de_senha_com_while copy/Verificador_de_senha_while.js
@@ -1,5 +1,25 @@
 let senhaCadastro = 12345;
 let senhaCorreta;
+
+                  //============//
+                  //Usando o !==//
+                  //============//
+while(senhaCorreta !== senhaCadastro){
+
+  senhaCorreta = parseInt(prompt("Digite sua senha: "))
+
+  if(senhaCorreta === senhaCadastro){
+    alert("Senha correta! ")
+  break
+  }else{
+      alert("Senha errada,tente de novo!")
+    }
+  }
+  
+                //===========//
+                //Usando true//
+                //===========//
+
 while(true){
 
   senhaCorreta = parseInt(prompt("Digite sua senha: "))
@@ -12,5 +32,5 @@ while(true){
     }
   }
   
-
+  
   
